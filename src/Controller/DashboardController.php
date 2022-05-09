3edaaf22b3c21ea9d21bf989c0420a6614bc8809
Stefan Kilibarda <stefandkilibarda@gmail.com>
2022-05-09 15:29:35 +0200

    login development

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('4df319b75e352ec2c7c3d2a02cffd9f9', name: 'dashboard_')]
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
+
+class DashboardController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    // #[IsGranted('ROLE_ADMIN')]
+    public function index(): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', "Role error", 'You must be moderator');
+
+        if(!$this->isGranted('ROLE_ADMIN'))
+            return $this->redirectToRoute('dashboard_logout');
+
+        return $this->render('dashboard/index.html.twig');
+    }
+}
---
+++ src/Controller/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+#[Route('4df319b75e352ec2c7c3d2a02cffd9f9', name: 'dashboard_')]
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+       if($this->getUser())
+        return $this->redirectToRoute('dashboard_index');
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        return $this->render('login/index.html.twig', [
+            'error' => $error
+        ]);
+    }
+}
